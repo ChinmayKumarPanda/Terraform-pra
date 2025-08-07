586f250c57ddd126f364295c00149a84876b8251
ChinmayKumarPanda <chinmaypanda447@email.com>
2025-08-07 14:25:03 +0530

    add dynamodb

--- DAY-4-state-locking/backend.tf
+++ DAY-4-state-locking/backend.tf
@@ -4,6 +4,8 @@ terraform {
     key    = "DAY-4/terraform.tfstate"
     region = "ap-south-1"
     profile = "TESTING"
-    use_lockfile = true #S3 supports the feature 
+    #use_lockfile = true #S3 supports the feature 
+    dynamodb_table = "Test"
+    encrypt = true
   }
 }
