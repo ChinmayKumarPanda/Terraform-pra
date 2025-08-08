94dfdbe104204264df217dd4e6ac4a16285439c2
ChinmayKumarPanda <chinmaypanda447@email.com>
2025-08-08 12:50:03 +0530

    update

--- DAY-5-Testing/backend.tf
+++ DAY-5-Testing/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mybuckets3demo00"
-    key = "DAY-4/terraform.tfstateclea"
+    key = "DAY-4/terraform.tfstate"
     region = "ap-south-1"
     profile = "TESTING"
     
