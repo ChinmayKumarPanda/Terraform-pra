8060b92b7dce5ee5b5133733c0069f52788824a3
ChinmayKumarPanda <chinmaypanda447@email.com>
2025-08-20 11:50:56 +0530

    day-8

---
+++ DAY-8-vpc-modules/main.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+ name = "my-vpc"
+ cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a","ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+
+ tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type = "t2.micro"
+
+  subnet_id     = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+}
